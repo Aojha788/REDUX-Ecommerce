fc513524a80325ce2a1002c4883cee2948a88072
Aojha788 <143265479+Aojha788@users.noreply.github.com>
2023-11-06 14:48:38 +0530

    Update About.js

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -9,23 +9,11 @@ const About = () => {
         <Row>
           <h1 className="py-3 text-center">About Us</h1>
           <p>
-            Redux Commerce Limited is a leading Ecommerce company established in
-            June 2022. Main focus of this company is to develop product as a
-            Product, by maintaining the security and global standard of
-            New-Ecommerce-Era. The main products of the company include Exchange
-            Broker/TREC holder's Back-Office Management System, Cloud based
-            Point of Sale and Enterprise Resource Planning solution, e-Commerce
-            Solution and Hotel Owners' Property Management System etc. The
-            management team of this company are the ICT Industry leaders having
-            more than 25 years experience in ICT arena.
+           
+          AVANISH OJHA COMPANY
           </p>
           <p>
-            The main products of the company include Exchange Broker/TREC
-            holder's Back-Office Management System, Cloud based Point of Sale
-            and Enterprise Resource Planning solution, e-Commerce Solution and
-            Hotel Owners' Property Management System etc. The management team of
-            this company are the ICT Industry leaders having more than 25 years
-            experience in ICT arena.
+           CODEGAMA PROJECT
           </p>
         </Row>
       </Container>
